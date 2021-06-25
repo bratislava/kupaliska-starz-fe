7c7a2fbd785a33b6e3b94b4a3899324d5d4d5b77
Matej Hospodar <matej.hospodar@student.tuke.sk>
2021-06-25 12:51:07 +0200

    remade hero banner

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -8,53 +8,58 @@ import { HashLink } from "react-router-hash-link";
 
 const HeroBanner = () => {
   return (
-    <div className="hero-container relative w-full flex items-center">
-      <div className="wave absolute inset-0 flex flex-col lg:justify-end justify-center">
-        <div className="h-9/10 xs:h-4/5 w-4/5 sm:h-3/5 sm:w-1/2 md:h-1/2 xl:h-3/5 bg-white lg:bg-transparent rounded-r-lg">
-          <div className="ml-container flex items-center md:items-start h-full pr-8 md:pt-12 xl:pt-0">
-            <div className="flex flex-col">
-              <div className="flex">
-                <div className="flex-1">
-                  <Typography
-                    type="title"
-                    fontWeight="bold"
-                    className="xl:mb-4"
-                  >
-                    Zakúp si lístok alebo permanentku
-                  </Typography>
-                  <Typography type="subtitle" className="xl:mb-4">
-                    a užívaj si leto na ktoromkoľvek z našich kúpalísk v
-                    Bratislave
-                  </Typography>
-                </div>
-                <div className="lg:flex-1/2" />
-              </div>
-              <div className="flex flex-col items-start lg:flex-row">
-                <HashLink to="/#ticket-buy" className="flex-1 w-full">
-                  <Button className="w-full my-3 lg:mt-4 lg:mr-2">
-                    <span className="xs:hidden">Chcem lístok</span>
-                    <span className="pr-2 lg:pr-1 xl:pr-2 hidden xs:block">
-                      Chcem lístok/permanentku
-                    </span>
-                    <Icon name="shopping-cart" className="hidden xs:block" />
-                  </Button>
-                </HashLink>
-                <HashLink to="/#swimming-pools" className="flex-1 w-full">
-                  <Button
-                    className="w-full mb-1 xs:mb-3 lg:mt-4 lg:ml-4 flex-1"
-                    type="outlined"
-                  >
-                    <span className="pr-2">Letné kúpaliská STaRZ</span>
-                    <Icon
-                      name="swimming-man"
-                      color="primary"
-                      className="hidden xs:block"
-                    />
-                  </Button>
-                </HashLink>
-              </div>
-            </div>
-          </div>
+    <div className="relative mb-8">
+      <div className="relative hero-image w-full">
+        <div className="wave w-full h-full absolute bottom-0"></div>
+      </div>
+      <div className="container mx-auto content relative z-10">
+        <Typography
+          type="title"
+          fontWeight="bold"
+          className="xl:mb-4 max-w-xs"
+        >
+          Kúp si lístok na ktorékoľvek kúpalisko v Bratislave online
+        </Typography>
+
+        <div
+          className="
+            flex
+            space-x-4
+            w-full
+            mt-8
+            mb-16
+
+            md:flex-col
+            md:space-y-4
+            md:space-x-0
+            md:w-1/2
+
+            lg:flex-row
+            lg:space-y-0
+            lg:space-x-4
+
+            xl:w-2/5
+          "
+        >
+          <HashLink to="/#ticket-buy" className="flex-1 w-full">
+            <Button className="w-full">
+              <span className="pr-4">Kúpiť lístok</span>
+              <Icon name="shopping-cart" />
+            </Button>
+          </HashLink>
+          <HashLink to="/#swimming-pools" className="hidden sm:block flex-1 w-full">
+            <Button
+              className="w-full flex-1"
+              type="outlined"
+            >
+              <span className="pr-2">Letné kúpaliská STaRZ</span>
+              <Icon
+                name="swimming-man"
+                color="primary"
+                className="hidden xs:block"
+              />
+            </Button>
+          </HashLink>
         </div>
       </div>
     </div>
