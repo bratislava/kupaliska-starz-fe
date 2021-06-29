5245679ded2ffe19b00bb024be8918f89b854761
Matej Hospodar <matejhospodar123@gmail.com>
2021-06-29 14:37:29 +0200

    suspense removed

--- src/App.tsx
+++ src/App.tsx
@@ -11,45 +11,43 @@ import { useAppDispatch, useAppSelector } from "hooks";
 import { initPageGlobalState, selectToast, setToast } from "store/global";
 
 const renderRoute = (route: IRoute) => (
-  <Route
-    key={route.path}
-    exact={route.exact}
-    path={route.path}
-    render={() => <route.component />}
-  />
+    <Route
+        key={route.path}
+        exact={route.exact}
+        path={route.path}
+        render={() => <route.component />}
+    />
 );
 
 function App() {
-  const dispatch = useAppDispatch();
-  const toast = useAppSelector(selectToast);
-  useEffect(() => {
-    dispatch(initPageGlobalState());
-  }, [dispatch]);
-  return (
-    <ConnectedRouter history={history}>
-      <React.Suspense fallback={<div></div>}>
-        <ScrollToTop>
-          <Toast
-            open={toast !== undefined}
-            type={toast?.type}
-            text={toast?.message}
-            onClose={() => {
-              dispatch(setToast(undefined));
-            }}
-            timeToClose={toast?.type === "success" ? 3000 : undefined}
-            closeButton={toast?.type !== "success"}
-          />
-          <TopBanner />
-          <main className="relative flex flex-col" style={{ flex: 1 }}>
-            <Header />
+    const dispatch = useAppDispatch();
+    const toast = useAppSelector(selectToast);
+    useEffect(() => {
+        dispatch(initPageGlobalState());
+    }, [dispatch]);
+    return (
+        <ConnectedRouter history={history}>
+            <ScrollToTop>
+                <Toast
+                    open={toast !== undefined}
+                    type={toast?.type}
+                    text={toast?.message}
+                    onClose={() => {
+                        dispatch(setToast(undefined));
+                    }}
+                    timeToClose={toast?.type === "success" ? 3000 : undefined}
+                    closeButton={toast?.type !== "success"}
+                />
+                <TopBanner />
+                <main className="relative flex flex-col" style={{ flex: 1 }}>
+                    <Header />
 
-            <Switch>{routes.map(renderRoute)}</Switch>
-          </main>
-          <Footer />
-        </ScrollToTop>
-      </React.Suspense>
-    </ConnectedRouter>
-  );
+                    <Switch>{routes.map(renderRoute)}</Switch>
+                </main>
+                <Footer />
+            </ScrollToTop>
+        </ConnectedRouter>
+    );
 }
 
 export default App;
