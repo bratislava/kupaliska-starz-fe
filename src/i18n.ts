cb143e9647803dca6f9760558c573c3f18e25773
Matej Hospodar <matejhospodar123@gmail.com>
2021-06-29 14:39:07 +0200

    host fixed

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,45 +1,44 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 // don't want to use this?
-// have a look at the Quick start guide 
+// have a look at the Quick start guide
 // for passing in lng and translations on init
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(Backend)
+    // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+    // learn more: https://github.com/i18next/i18next-http-backend
+    // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(Backend)
 
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        backend: {
+            loadPath: `http://${window.location.host}/locales/{{lng}}/{{ns}}.json`,
+        },
 
-    backend: {
-        loadPath: 'http://localhost:3000/locales/{{lng}}/{{ns}}.json'
-    },
+        detection: {
+            order: ["path"],
+            lookupQuerystring: "lang",
+            lookupFromPathIndex: 0,
+        },
 
-    detection: {
-        order: ['path'],
-        lookupQuerystring: 'lang',
-        lookupFromPathIndex: 0,
-    },
+        supportedLngs: ["sk", "en"],
+        fallbackLng: "sk",
 
-    supportedLngs: ['sk', 'en'],
-    fallbackLng: 'sk',
+        debug: true,
 
-    debug: true,
-
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-});
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+    });
 
 export default i18n;
