2de1cac80186793f7d6e1121392f7a2db397a3b0
Matej Hospodar <matejhospodar123@gmail.com>
2021-07-06 14:44:38 +0200

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,10 @@
 // in CI build these env vars are not defined, and the %{TOKEN}% is replaced post build based on env
 
 export const environment = {
-  baseUrl: process.env.REACT_APP_HOST || "%{HOST}%",
+  protocol: "http:",
+  port: "8000",
+  host: "localhost",
   debug: false,
   maxTicketPuchaseLimit: 10,
-  reCaptchaKey: process.env.REACT_APP_RECAPTCHA_CLIENT_SECRET || "%{RECAPTCHA_CLIENT_SECRET}%"
+  reCaptchaKey: "6Leae90aAAAAAM7NZfVEVC0rEyhh-tx-MYUthzYU"
 };
