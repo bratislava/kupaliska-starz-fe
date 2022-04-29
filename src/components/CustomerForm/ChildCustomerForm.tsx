38476b5dd0daa718dbcce98acd771baa8d87b099
Adam Grund <adam.grund@gmail.com>
2022-04-29 11:26:22 +0200

    refactoring - add new component for images

--- src/components/CustomerForm/ChildCustomerForm.tsx
+++ src/components/CustomerForm/ChildCustomerForm.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from "react";
-
-import { Button, Icon, InputField, Typography } from "components";
+import React, { useEffect } from "react";
+import { Icon, InputField } from "components";
 import { get } from "lodash";
-import Resizer from "react-image-file-resizer";
 import { useTranslation } from "react-i18next";
+
+import PhotoField from "components/PhotoField/PhotoField";
 
 interface ChildCustomerFormProps {
   register: any;
@@ -34,7 +34,6 @@ const ChildCustomerForm = ({
   childPhotoRequired = true,
   onClear,
 }: ChildCustomerFormProps) => {
-  const imageInputRef = useRef<HTMLInputElement | null>(null);
   const _errors = fieldNamePrefix
     ? get(errors, `${fieldNamePrefix ? fieldNamePrefix : ""}`)
     : errors;
@@ -57,71 +56,6 @@ const ChildCustomerForm = ({
       });
   }, [register, fieldNamePrefix]);
 
-  const handleImageFile = (file: any) => {
-    if (file !== undefined) {
-      if (file.size > 5242880) {
-        setError(fieldNamePrefix ? fieldNamePrefix + ".photo" : "photo", {
-          message: "Veľkosť fotky musí byť menej ako 5MB",
-        });
-        return;
-      }
-      clearErrors(fieldNamePrefix ? fieldNamePrefix + ".photo" : "photo");
-      Resizer.imageFileResizer(
-        file,
-        400,
-        400,
-        "JPEG",
-        50,
-        0,
-        (uri) => {
-          // because performance issues when base64 img is in form
-          onPhotoSet && onPhotoSet(uri ? (uri as string) : "");
-          setValue &&
-            uri &&
-            setValue(
-              fieldNamePrefix ? fieldNamePrefix + ".photo" : "photo",
-              "set"
-            );
-        },
-        "base64"
-      );
-    }
-  };
-
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file =
-      e.target.files && e.target.files.length ? e.target.files[0] : undefined;
-
-    file && handleImageFile(file);
-  };
-
-  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  const onDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-    const file =
-      event.dataTransfer.files && event.dataTransfer.files.length
-        ? event.dataTransfer.files[0]
-        : undefined;
-    if (file) {
-      handleImageFile(file);
-    }
-  };
-
-  const openImageInput = () => {
-    if (imageInputRef && imageInputRef.current) {
-      imageInputRef.current.click();
-    }
-  };
   return (
     <div className={`grid grid-cols-2 gap-4 lg:grid-cols-4 ${className}`}>
       <div className="col-span-2 flex">
@@ -166,70 +100,15 @@ const ChildCustomerForm = ({
         </div>
       )}
       {childPhotoRequired && (
-        <div className="col-span-full grid gap-x-4 grid-cols-3">
-          <div className="col-span-1">
-            {image ? (
-              <div
-                className="square overflow-hidden bg-center bg-cover bg-no-repeat"
-                style={{ backgroundImage: `url(${image})` }}
-              />
-            ) : (
-              <>
-                <div
-                  onClick={openImageInput}
-                  className={`${
-                    get(_errors, "photo.message")
-                      ? "border-error border-2 border-solid"
-                      : "border-2-softGray"
-                  } w-full rounded-lg`}
-                  onDragOver={onDragOver}
-                  onDragEnter={onDragEnter}
-                  onDrop={onDrop}
-                >
-                  <div className="w-5/10 mx-auto">
-                    <img
-                      className="p-4 w-full"
-                      src="/download-file.svg"
-                      alt="User face placeholder"
-                    />
-                  </div>
-                  <div className="p-4 text-sm text-fontBlack text-opacity-50 text-center">
-                    {t("buy-page.photo-click")}
-                  </div>
-                </div>
-                {get(_errors, "photo.message") && (
-                  <div className="text-error">
-                    {get(_errors, "photo.message")}
-                  </div>
-                )}
-              </>
-            )}
-          </div>
-          <div className="col-span-2">
-            <Typography type="subtitle" fontWeight="medium">
-              {t("buy-page.photo-title")}
-            </Typography>
-            <p className="leading-tight md:leading-normal">
-              {t("buy-page.photo-description")}
-            </p>
-            <input
-              ref={imageInputRef}
-              type="file"
-              hidden
-              accept=".jpg,.png,.jpeg"
-              onChange={handleImageChange}
-            />
-            <div className="text-sm my-2">{t("buy-page.max-size")}</div>
-            <Button
-              thin
-              rounded
-              className="w-full lg:w-1/2 mb-4"
-              onClick={openImageInput}
-            >
-              {t("buy-page.photo-upload")}
-            </Button>
-          </div>
-        </div>
+        <PhotoField
+          fieldNamePrefix={fieldNamePrefix}
+          setValue={setValue}
+          setError={setError}
+          clearErrors={clearErrors}
+          errors={_errors}
+          onPhotoSet={onPhotoSet}
+          image={image}
+        />
       )}
     </div>
   );
--- src/components/CustomerForm/CustomerForm.tsx
+++ src/components/CustomerForm/CustomerForm.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
 import { get } from "lodash";
-import Resizer from "react-image-file-resizer";
 
-import { Button, Icon, InputField, Tooltip, Typography } from "components";
+import { Icon, InputField, Tooltip } from "components";
 import { useTranslation } from "react-i18next";
+import PhotoField from "components/PhotoField/PhotoField";
 
 interface CustomerFormProps {
   register: any;
@@ -33,7 +33,6 @@ const CustomerForm = ({
   onPhotoSet,
   image,
 }: CustomerFormProps) => {
-  const imageInputRef = useRef<HTMLInputElement | null>(null);
   const _errors = fieldNamePrefix
     ? get(errors, `${fieldNamePrefix ? fieldNamePrefix : ""}`)
     : errors;
@@ -46,71 +45,6 @@ const CustomerForm = ({
       });
   }, [register, fieldNamePrefix]);
 
-  const handleImageFile = (file: any) => {
-    if (file.size > 5242880) {
-      setError(fieldNamePrefix ? fieldNamePrefix + ".photo" : "photo", {
-        message: t("buy-page.max-size-error"),
-      });
-      return;
-    }
-    clearErrors(fieldNamePrefix ? fieldNamePrefix + ".photo" : "photo");
-    Resizer.imageFileResizer(
-      file,
-      400,
-      400,
-      "JPEG",
-      50,
-      0,
-      (uri) => {
-        // because performance issues when base64 img is in form
-        onPhotoSet && onPhotoSet(uri ? (uri as string) : "");
-        setValue &&
-          uri &&
-          setValue(
-            fieldNamePrefix ? fieldNamePrefix + ".photo" : "photo",
-            "set"
-          );
-      },
-      "base64"
-    );
-  };
-
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file =
-      e.target.files && e.target.files.length ? e.target.files[0] : undefined;
-
-    if (file) {
-      handleImageFile(file);
-    }
-  };
-
-  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  const onDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-    const file =
-      event.dataTransfer.files && event.dataTransfer.files.length
-        ? event.dataTransfer.files[0]
-        : undefined;
-    if (file) {
-      handleImageFile(file);
-    }
-  };
-
-  const openImageInput = () => {
-    if (imageInputRef && imageInputRef.current) {
-      imageInputRef.current.click();
-    }
-  };
   return (
     <>
       <Tooltip multiline={true} id="tooltip-customer-form" />
@@ -136,70 +70,15 @@ const CustomerForm = ({
         />
         {!nameRequired && <div className="col-span-1" />}
         {photoRequired && (
-          <div className="col-span-full grid gap-x-4 grid-cols-3">
-            <div className="col-span-1">
-              {image ? (
-                <div
-                  className="square overflow-hidden bg-center bg-cover bg-no-repeat"
-                  style={{ backgroundImage: `url(${image})` }}
-                />
-              ) : (
-                <>
-                  <div
-                    onClick={openImageInput}
-                    className={`${
-                      get(_errors, "photo.message")
-                        ? "border-error border-2 border-solid"
-                        : "border-2-softGray"
-                    } w-full rounded-lg`}
-                    onDrop={onDrop}
-                    onDragOver={onDragOver}
-                    onDragEnd={onDragEnter}
-                  >
-                    <div className="w-5/10 mx-auto">
-                      <img
-                        className="p-4 w-full"
-                        src="/download-file.svg"
-                        alt="User face placeholder"
-                      />
-                    </div>
-                    <div className="p-4 text-sm text-fontBlack text-opacity-50 text-center">
-                      {t("buy-page.photo-click")}
-                    </div>
-                  </div>
-                  {get(_errors, "photo.message") && (
-                    <div className="text-error">
-                      {get(_errors, "photo.message")}
-                    </div>
-                  )}
-                </>
-              )}
-            </div>
-            <div className="col-span-2">
-              <Typography type="subtitle" fontWeight="medium">
-                {t("buy-page.photo-title")}
-              </Typography>
-              <p className="leading-tight text-sm md:leading-normal md:text-base">
-                {t("buy-page.photo-description")}
-              </p>
-              <input
-                ref={imageInputRef}
-                type="file"
-                accept=".jpg,.jpeg,.png"
-                hidden
-                onChange={handleImageChange}
-              />
-              <div className="text-sm my-2">{t("buy-page.max-size")}</div>
-              <Button
-                thin
-                rounded
-                className="w-full lg:w-1/2 mb-4"
-                onClick={openImageInput}
-              >
-                {t("buy-page.photo-upload")}
-              </Button>
-            </div>
-          </div>
+          <PhotoField
+            fieldNamePrefix={fieldNamePrefix}
+            setValue={setValue}
+            setError={setError}
+            clearErrors={clearErrors}
+            errors={_errors}
+            onPhotoSet={onPhotoSet}
+            image={image}
+          />
         )}
         <div className="col-span-1">
           <label className="font-medium text-fontBlack text-opacity-50 flex items-center my-2">
