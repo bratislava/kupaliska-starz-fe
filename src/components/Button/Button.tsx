f896297ce4992d42d668a34469b91727e54b3bc7
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-05-06 13:25:38 +0200

    KUP-20 Button fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -24,12 +24,27 @@ const Button = ({
   htmlType = "button",
 }: PropsWithChildren<ButtonProps>) => {
   // tailwind purges values that are only interpolated in so i have to do this bad way
-  const textColor = { primary: 'text-white', secondary: 'text-secondary"', outlined: "text-primary", blueish: "text-primary" }[color]
-  const bgColor = { primary: 'bg-primary', secondary: 'bg-secondary"', outlined: "bg-transparent", blueish: "bg-blueish" }[color]
-  const borderColor = { primary: 'border-primary', secondary: 'border-secondary"', outlined: "border-primary", blueish: "border-blueish" }[color]
+  const textColor = {
+    primary: "text-white",
+    secondary: 'text-secondary"',
+    outlined: "text-primary",
+    blueish: "text-primary",
+  }[color];
+  const bgColor = {
+    primary: "bg-primary",
+    secondary: 'bg-secondary"',
+    outlined: "bg-transparent",
+    blueish: "bg-blueish",
+  }[color];
+  const borderColor = {
+    primary: "border-primary",
+    secondary: 'border-secondary"',
+    outlined: "border-primary",
+    blueish: "border-blueish",
+  }[color];
   const thinClass = thin ? "p-1" : "p-2";
   const roundedClass = rounded ? "rounded-lg" : "rounded";
-  const classNames = `flex justify-center items-center focus:outline-none border-solid border-2 ${textColor} ${bgColor} ${borderColor} ${thinClass} ${roundedClass}`
+  const classNames = `flex justify-center items-center focus:outline-none border-solid border-2 ${textColor} ${bgColor} ${borderColor} ${thinClass} ${roundedClass}`;
 
   return (
     <button
