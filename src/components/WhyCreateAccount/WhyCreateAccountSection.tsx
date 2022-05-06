69084ad45ba764aa80dc4ec6ba140bb8d18a2ac3
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-05-06 15:01:02 +0200

    KUP-20 Add why create account section

--- src/components/WhyCreateAccount/WhyCreateAccountSection.tsx
+++ src/components/WhyCreateAccount/WhyCreateAccountSection.tsx
@@ -3,25 +3,33 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const WhyCreateAccountSection = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return <section id="why-create-account" className="section">
-        <SectionHeader title="Prečo si založiť účet?" className="text-center"/>
-        {[0,1,2].map((index) => <div
-            className={`flex lg:w-8/10 mx-auto flex-col-reverse mb-10 md:mb-0 ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'}`}
-            key={index}>
-            <div className="flex-1 flex justify-center items-center text-center mt-6 md:mt-0">
-                <div className="w-80">
-                    <div className="font-semibold text-2xl mb-6">{t(`landing.why-create-account.${index}.title`)}</div>
-                    <p>{t(`landing.why-create-account.${index}.content`)}</p>
-                </div>
+  return (
+    <section id="why-create-account" className="section">
+      <SectionHeader title="Prečo si založiť účet?" className="text-center" />
+      {[0, 1, 2].map((index) => (
+        <div
+          className={`flex lg:w-8/10 mx-auto flex-col-reverse mb-10 md:mb-0 ${
+            index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
+          }`}
+          key={index}
+        >
+          <div className="flex-1 flex justify-center items-center text-center mt-6 md:mt-0">
+            <div className="w-80">
+              <div className="font-semibold text-2xl mb-6">
+                {t(`landing.why-create-account.${index}.title`)}
+              </div>
+              <p>{t(`landing.why-create-account.${index}.content`)}</p>
             </div>
-            <div className="flex flex-none justify-center"><img
-                alt=""
-                src={`phone-${index + 1}.png`}
-            /></div>
-        </div>)}
-    </section>;
+          </div>
+          <div className="flex flex-none justify-center">
+            <img alt="" src={`phone-${index + 1}.png`} />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default WhyCreateAccountSection;
