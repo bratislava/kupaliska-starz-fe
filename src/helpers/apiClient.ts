36a69dcf579571f099ae3a6e22a5ae84b2383026
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-06-07 15:06:58 +0200

    Create api clients with MSAL

--- src/helpers/apiClient.ts
+++ src/helpers/apiClient.ts
@@ -1,20 +1,64 @@
-import axios from "axios";
+import axios, { AxiosRequestHeaders } from "axios";
 import qs from "qs";
-
 import { environment } from "../environment";
+import { getMsalIdToken } from "./getMsalIdToken";
+import { msalInstance } from "../msalInstance";
 
-export const apiClient = axios.create({
-  responseType: "json",
-  baseURL: environment.host,
-  withCredentials: true,
-  paramsSerializer: (params: any) => {
-    return qs.stringify(params);
-  },
-  timeout: 100000,
-});
+enum WithMsal {
+  None,
+  Required,
+  IfAvailable,
+}
 
-apiClient.interceptors.request.use((config) => {
-  config.headers.common["Cache-Control"] = "no-cache";
+const createApiClient = (withMsal = WithMsal.None) => {
+  const newApiClient = axios.create({
+    responseType: "json",
+    baseURL: environment.host,
+    withCredentials: true,
+    paramsSerializer: (params: any) => {
+      return qs.stringify(params);
+    },
+    timeout: 100000,
+  });
 
-  return config;
-});
+  newApiClient.interceptors.request.use((config) => {
+    (config.headers as AxiosRequestHeaders)["Cache-Control"] = "no-cache";
+
+    return config;
+  });
+
+  if (withMsal === WithMsal.Required) {
+    newApiClient.interceptors.request.use(async (config) => {
+      const msalIdToken = await getMsalIdToken();
+      (config.headers as AxiosRequestHeaders)[
+        "Authorization"
+      ] = `Bearer ${msalIdToken}`;
+
+      return config;
+    });
+  }
+
+  if (withMsal === WithMsal.IfAvailable) {
+    newApiClient.interceptors.request.use(async (config) => {
+      const hasAccount = msalInstance.getActiveAccount();
+      if (!hasAccount) {
+        return config;
+      }
+
+      const msalIdToken = await getMsalIdToken();
+      (config.headers as AxiosRequestHeaders)[
+        "Authorization"
+      ] = `Bearer ${msalIdToken}`;
+
+      return config;
+    });
+  }
+
+  return newApiClient;
+};
+
+export const apiClient = createApiClient();
+export const apiClientWithMsal = createApiClient(WithMsal.Required);
+export const apiClientWithMsalIfAvailable = createApiClient(
+  WithMsal.IfAvailable
+);
