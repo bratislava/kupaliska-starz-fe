8f49e93f3d8b7eb502e29dfc3567d734f9eded09
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-06-07 15:01:14 +0200

    Add new features to modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,21 +1,74 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useMemo } from "react";
+import { useElementSize, useLockedBody } from "usehooks-ts";
+import FocusTrap from "focus-trap-react";
+import cx from "classnames";
+
+import "./Modal.css";
+
+const ModalButton = ({ button }: { button: React.ReactNode }) => {
+  const [contentRef, { height: contentHeight }] = useElementSize();
+
+  const height = useMemo(() => contentHeight / 2, [contentHeight]);
+
+  return (
+    <div style={{ height }}>
+      <div className="transform -translate-y-2/4" ref={contentRef}>
+        {button}
+      </div>
+    </div>
+  );
+};
+
 interface ModalProps {
   open: boolean;
   onClose?: () => void;
+  button?: React.ReactNode;
+  modalClassName?: string;
+  closeButton?: boolean;
 }
-const Modal = ({ open, onClose, children }: PropsWithChildren<ModalProps>) => {
+const Modal = ({
+  open,
+  onClose = () => {},
+  children,
+  button,
+  modalClassName,
+  closeButton = false,
+}: PropsWithChildren<ModalProps>) => {
+  useLockedBody(open);
+
   return open ? (
-    <div
-      id="modal-backdrop"
-      onClick={(event: React.MouseEvent<HTMLDivElement>) => {
-        if ((event.target as Element).id === "modal-backdrop") {
-          onClose && onClose();
-        }
-      }}
-      className="fixed inset-0 flex flex-col justify-center items-center bg-fontBlack bg-opacity-30 z-50"
-    >
-      {children}
-    </div>
+    <FocusTrap>
+      <div
+        id="modal-backdrop"
+        onClick={(event: React.MouseEvent<HTMLDivElement>) => {
+          if ((event.target as Element).id === "modal-backdrop") {
+            onClose();
+          }
+        }}
+        className="fixed inset-0 flex flex-col justify-center items-center bg-fontBlack bg-opacity-30 z-50"
+      >
+        <div className={cx("relative", { "px-4": closeButton })}>
+          <div
+            className={cx(
+              "relative bg-white rounded-lg shadow-lg overflow-y-auto",
+              modalClassName
+            )}
+          >
+            {children}
+          </div>
+          {closeButton && (
+            <img
+              src="/red-cross-circle.svg"
+              alt=""
+              className="absolute right-0 top-0 transform -translate-y-2/4 w-8 h-8 cursor-pointer"
+              onClick={() => onClose()}
+            />
+          )}
+        </div>
+
+        {button && <ModalButton button={button}></ModalButton>}
+      </div>
+    </FocusTrap>
   ) : null;
 };
 
