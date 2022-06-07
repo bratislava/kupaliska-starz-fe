3336a7330d217f32c4f75e94eee439c1ba84ca62
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-06-07 14:29:35 +0200

    Footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Footer = () => (
-  <div className="w-full h-25 bg-blueish">
+  <div className="w-full h-25 bg-blueish mt-12">
     <div className="w-full h-full flex items-center justify-center">
-      <div className="text-primary font-semibold text-lg">STARZ © 2021</div>
+      <div className="text-primary font-semibold text-lg">STARZ © 2022</div>
     </div>
   </div>
 );
