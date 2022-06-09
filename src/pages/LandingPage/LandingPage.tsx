e5478fa8972264edebaca6d4502ea7e563a606de
Adam Grund <adam.grund@gmail.com>
2022-06-09 17:49:39 +0200

    faq text

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -45,7 +45,9 @@ const items = [
   },
 ];
 
-const faqsn = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const faqsn = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+];
 
 const LandingPage = () => {
   const [allSwimmingPools, setAllSwimmingPools] = useState<boolean>(false);
