5ac6a34360fe2c59b4e59f031f92325ec01f6144
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-06-14 12:43:04 +0200

    Add comment

--- src/helpers/getObjectChanges.ts
+++ src/helpers/getObjectChanges.ts
@@ -1,5 +1,6 @@
 import { differenceWith, fromPairs, isEqual, toPairs } from "lodash";
 
+// https://stackoverflow.com/a/71714850
 export function getObjectChanges<T extends object, K extends object>(obj1: T, obj2: K) {
   return fromPairs(differenceWith(toPairs(obj2), toPairs(obj1), isEqual)) as T & K;
 }
