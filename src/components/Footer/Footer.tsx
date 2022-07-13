eafb05b11948316e40615c20b40092b82a6efa20
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-07-13 16:30:49 +0200

    Fix footer text wrap

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
 
   return (
     <div className="w-full bg-blueish mt-4 md:mt-24">
-      <div className="w-full flex items-center justify-between py-4 grid grid-cols-1 md:grid-cols-3 container mx-auto">
-        <div className="flex flex-col text-primary order-1 my-2 md:my-0 md:order-1">
+      <div className="w-full flex items-center justify-between py-4 grid grid-cols-1 lg:grid-cols-3 container mx-auto">
+        <div className="flex flex-col text-primary order-1 my-2 lg:my-0 lg:order-1">
           <span className="font-semibold">{t("common.contact")}</span>
           <span>
             Správa telovýchovných a rekreačných zariadení hlavného mesta
@@ -19,7 +19,7 @@ const Footer = () => {
           <span>IČO: 00179663</span>
           <span>DIČ: 2020801695</span>
         </div>
-        <div className="text-primary flex items-center order-3 md:order-2 justify-center col-span-1">
+        <div className="text-primary flex items-center order-3 lg:order-2 justify-center col-span-1">
           STARZ |{" "}
           <a
             href="https://inovacie.bratislava.sk"
@@ -31,7 +31,7 @@ const Footer = () => {
           </a>
           | 2022
         </div>
-        <div className="flex flex-col text-primary md:items-end order-2 md:order-3 my-2 md:my-0">
+        <div className="flex flex-col text-primary lg:items-end order-2 lg:order-3 my-2 lg:my-0">
           <span className="font-semibold">{t("common.important-info")}</span>
           <Link className="link" to="/vop">
             {t("common.vop")}
