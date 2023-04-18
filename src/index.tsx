c22c593ae939f1a852be9de5df140b74213c8e23
Marek Bodinger <marek.bodinger@gmail.com>
2023-04-18 15:15:26 +0200

    Remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react'
 
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import * as serviceWorker from './serviceWorker'
 
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
@@ -38,8 +37,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
