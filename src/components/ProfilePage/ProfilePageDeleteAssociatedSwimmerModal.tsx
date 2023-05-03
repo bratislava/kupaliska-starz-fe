07face1011820f47b3e547e1389c775b6001070f
Marek Bodinger <marek.bodinger@gmail.com>
2023-05-03 16:49:55 +0200

    Profile page columns

---
+++ src/components/ProfilePage/ProfilePageDeleteAssociatedSwimmerModal.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {
+  AssociatedSwimmer,
+  AssociatedSwimmerFetchResponse,
+  deleteAssociatedSwimmer,
+} from '../../store/associatedSwimmers/api'
+import { useTranslation } from 'react-i18next'
+import { useMutation, useQueryClient } from 'react-query'
+import { AxiosResponse } from 'axios'
+import { produce } from 'immer'
+import Dialog from '../Dialog/Dialog'
+import { Button } from '../index'
+import Photo from '../Photo/Photo'
+
+type ProfilePageDeleteAssociatedSwimmerModalProps = {
+  swimmer: AssociatedSwimmer
+  onClose: () => void
+}
+
+const ProfilePageDeleteAssociatedSwimmerModal = ({
+  swimmer,
+  onClose,
+}: ProfilePageDeleteAssociatedSwimmerModalProps) => {
+  const { t } = useTranslation()
+  const queryClient = useQueryClient()
+  const mutation = useMutation(() => deleteAssociatedSwimmer(swimmer.id!), {
+    onSuccess: () => {
+      // Update data to see edited content before the refetch.
+      queryClient.setQueryData<AxiosResponse<AssociatedSwimmerFetchResponse> | undefined>(
+        'associatedSwimmers',
+        (old) => {
+          if (!old) {
+            return
+          }
+
+          return produce(old, (draft) => {
+            draft.data.associatedSwimmers = old.data.associatedSwimmers.filter(
+              (swimmerFromList) => swimmerFromList.id !== swimmer.id,
+            )
+          })
+        },
+      )
+      queryClient.invalidateQueries('associatedSwimmers')
+      onClose()
+    },
+  })
+  const handleRemove = () => {
+    mutation.mutate()
+  }
+
+  return (
+    <Dialog
+      title="Odstrániť z profilu"
+      open={true}
+      onClose={onClose}
+      footerButton={<Button onClick={handleRemove}>Odstrániť</Button>}
+      className="max-w-[488px]"
+    >
+      <div className="flex flex-col gap-4">
+        <span>Naozaj chcete odstrániť túto osobu z vášho profilu?</span>
+        <div className="flex flex-col gap-1 items-center">
+          <Photo size="normal" photo={swimmer.image} />
+          <div className="mt-2">
+            {swimmer.firstname} {swimmer.lastname}
+          </div>
+          <div>{t('person-component.age', { age: swimmer.age })}</div>
+        </div>
+      </div>
+    </Dialog>
+  )
+}
+
+export default ProfilePageDeleteAssociatedSwimmerModal
---
+++ src/components/ProfilePage/ProfilePageUser.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react'
+import { Button, Icon, Spinner } from '../index'
+import { useQuery } from 'react-query'
+import { fetchUser } from '../../store/user/api'
+import { useAccount } from '@azure/msal-react'
+import ProfilePageAgeZipModal from './ProfilePageAgeZipModal'
+import { Button as AriaButton } from 'react-aria-components'
+import ProfilePagePhotoModal from './ProfilePagePhotoModal'
+import Photo from '../Photo/Photo'
+
+const ProfilePageUser = () => {
+  const { data, isLoading, isError } = useQuery('user', fetchUser)
+  const account = useAccount()
+  const [isAgeModalOpen, setIsAgeModalOpen] = useState(false)
+  const [isZipModalOpen, setIsZipModalOpen] = useState(false)
+  const [isPhotoModalOpen, setIsPhotoModalOpen] = useState(false)
+
+  return (
+    <>
+      {data && (
+        <>
+          {isAgeModalOpen && (
+            <ProfilePageAgeZipModal
+              type="age"
+              user={data.data}
+              onClose={() => setIsAgeModalOpen(false)}
+            />
+          )}
+          {isZipModalOpen && (
+            <ProfilePageAgeZipModal
+              type="zip"
+              user={data.data}
+              onClose={() => setIsZipModalOpen(false)}
+            />
+          )}
+          {isPhotoModalOpen && (
+            <ProfilePagePhotoModal user={data.data} onClose={() => setIsPhotoModalOpen(false)} />
+          )}
+        </>
+      )}
+      <div className="bg-white rounded-lg lg:col-span-7">
+        <div className="px-6 py-4 gap-6 flex items-start sm:items-center border-b-2 border-b-divider flex-col sm:flex-row">
+          <div className="flex flex-1 flex-col gap-1">
+            <p className="font-semibold text-xl">Osobné údaje</p>
+            <p>
+              Osobné údaje ako e-mail a vaše meno s priezviskom môžete spravovať v Bratislavskom
+              konte.
+            </p>
+          </div>
+          <Button>
+            <Icon className="mr-2 no-fill" name="castle" /> Spravovať v konte
+          </Button>
+        </div>
+        <div className="gap-6 flex flex-col p-6">
+          <div className="gap-6 flex flex-col sm:flex-row">
+            <Photo photo={data?.data.image} size="normal" />
+            <div className="flex flex-col gap-4">
+              <div className="gap-1 flex flex-col">
+                <p className="font-semibold">Fotografia</p>
+                <p className="text-sm">
+                  Pre kúpu permanentky je potrebné zadať aj fotografiu.
+                  <br />
+                  Tá slúži na priradenie permanentky k jej majiteľovi.
+                </p>
+              </div>
+              <Button
+                className="self-start"
+                color="outlined"
+                disabled={isLoading || isError}
+                onClick={() => setIsPhotoModalOpen(true)}
+              >
+                <Icon className="mr-2 no-fill" name="upload" /> Nahrať fotku
+              </Button>
+            </div>
+          </div>
+          <div className="h-0.5 bg-divider" />
+          <div className="gap-4 flex flex-col">
+            <div className="gap-2 sm:gap-6 flex flex-col sm:flex-row">
+              <div className="gap-2 flex items-center font-semibold sm:w-[200px]">
+                <p>Meno a priezvisko</p>
+              </div>
+              <p>
+                {account?.idTokenClaims?.given_name} {account?.idTokenClaims?.family_name}
+              </p>
+            </div>
+            <div className="gap-2 sm:gap-6 flex flex-col sm:flex-row">
+              <div className="gap-2 flex items-center font-semibold sm:w-[200px]">
+                <p>E-mail</p>
+              </div>
+              <p>{account?.username}</p>
+            </div>
+          </div>
+          <div className="h-0.5 bg-divider" />
+          {isLoading && (
+            <div className="flex justify-center p-6">
+              <Spinner />
+            </div>
+          )}
+          {data && (
+            <>
+              <div className="gap-2 sm:gap-6 flex flex-col sm:flex-row">
+                <div className="gap-2 flex items-center font-semibold sm:w-[200px]">
+                  <p>Vek</p>
+                </div>
+                <div className="flex grow">
+                  <p className="grow">{data.data.age}</p>
+                  <AriaButton onPress={() => setIsAgeModalOpen(true)} aria-label="Upraviť vek">
+                    <Icon name="pencil" className="no-fill" />
+                  </AriaButton>
+                </div>
+              </div>
+              <div className="gap-2 sm:gap-6 flex flex-col sm:flex-row">
+                <div className="gap-2 flex items-center font-semibold sm:w-[200px]">
+                  <p>PSČ</p>
+                </div>
+                <div className="flex grow">
+                  <p className="grow">{data.data.zip}</p>
+                  <AriaButton onPress={() => setIsZipModalOpen(true)} aria-label="Upraviť PSČ">
+                    <Icon name="pencil" className="no-fill" />
+                  </AriaButton>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProfilePageUser
