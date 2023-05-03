61bdc3702565de91ec2ecc095fcc6f9ee41b3fff
Adam Grund <adam.grund@gmail.com>
2023-05-03 09:27:23 +0200

    change recaptcha name

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -94,7 +94,7 @@ const ContactForm = () => {
           <>
             <Turnstile
               theme="light"
-              sitekey={import.meta.env.VITE_CLOUDFLARE_TURNSTILE_SITE_KEY ?? ''}
+              sitekey={import.meta.env.VITE_RECAPTCHA_CLIENT_SECRET ?? ''}
               onVerify={(token) => {
                 setCaptchaWarning('hide')
                 onChange(token)
