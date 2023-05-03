91b6c543b55cf48f54482669874cbe241d66d343
Marek Bodinger <marek.bodinger@gmail.com>
2023-05-03 16:50:01 +0200

    Profile page

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ProfileNavBar } from '../../components'
+import ProfilePageUser from '../../components/ProfilePage/ProfilePageUser'
+import ProfilePageSwimmers from '../../components/ProfilePage/ProfilePageSwimmers'
+import ProfileLine from '../../components/ProfileLine/ProfileLine'
+
+const ProfilePage = () => {
+  return (
+    <section className="w-full">
+      <div className="container mx-auto">
+        <ProfileNavBar></ProfileNavBar>
+        <ProfileLine></ProfileLine>
+        <div className="flex flex-col py-8 gap-8 mt-4">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-start">
+            <ProfilePageUser />
+            <ProfilePageSwimmers />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProfilePage
