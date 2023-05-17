184a058c8ed6bdcf553da5807398071f36497f4b
Marek Bodinger <marek.bodinger@gmail.com>
2023-05-17 14:43:02 +0200

    Use modal in useLogin

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,3 +1,31 @@
 import { redirectToLogin } from 'helpers/cityAccountApi'
+import { useLocalStorage } from 'usehooks-ts'
+import { environment } from '../environment'
+import { useCityAccountLoginRedirectionModal } from '../components/CityAccountLoginInformationModal/CityAccountLoginRedirectionModal'
 
-export const useLogin = () => async () => redirectToLogin()
+const redirectionModalConfirmedKey = 'redirectionModalConfirmed'
+
+export const useLogin = () => {
+  const [redirectionModalConfirmed, setRedirectionModalConfirmed] = useLocalStorage(
+    redirectionModalConfirmedKey,
+    false,
+  )
+  const { open } = useCityAccountLoginRedirectionModal()
+
+  return (fromUrl?: string) => {
+    if (
+      environment.featureFlag.showCityAccountLoginInformationModalOnce &&
+      redirectionModalConfirmed
+    ) {
+      redirectToLogin(fromUrl)
+      return
+    }
+
+    open(() => {
+      if (environment.featureFlag.showCityAccountLoginInformationModalOnce) {
+        setRedirectionModalConfirmed(true)
+      }
+      redirectToLogin(fromUrl)
+    })
+  }
+}
