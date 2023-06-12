a57169bb07659b645c4bd586f6e8244805abe190
Adam Grund <adam.grund@gmail.com>
2023-06-12 15:54:51 +0200

    [#105] removed usless code

--- src/components/SwimmingPoolInfoCard/SwimmingPoolInfoCard.tsx
+++ src/components/SwimmingPoolInfoCard/SwimmingPoolInfoCard.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { SwimmingPool } from 'models'
 
 import { Typography, Spinner } from 'components'
 
 import './SwimmingPoolInfoCard.css'
-import { useWindowSize } from 'hooks'
 import { useTranslation } from 'react-i18next'
 
 interface SwimmingPoolInfoCardProps {
@@ -14,15 +13,7 @@ interface SwimmingPoolInfoCardProps {
 }
 
 const SwimmingPoolInfoCard = ({ swimmingPool, className = '' }: SwimmingPoolInfoCardProps) => {
-  const [_expanded, setExpanded] = useState<boolean>(false)
-  const { width } = useWindowSize()
   const { t } = useTranslation()
-
-  useEffect(() => {
-    if (width && width >= 768) {
-      setExpanded(false)
-    }
-  }, [width])
 
   return (
     <div className={`${className} grid bg-backgroundGray rounded-lg swimming-pool-card__container`}>
@@ -32,17 +23,14 @@ const SwimmingPoolInfoCard = ({ swimmingPool, className = '' }: SwimmingPoolInfo
           src={swimmingPool.image ? swimmingPool.image.originalFile : '/kupalisko-delfin.png'}
           alt={swimmingPool.image ? swimmingPool.image.altText : t('landing.alt-img-text')}
         />
-        {/* <div className={'wave-overlay absolute inset-0'} /> */}
       </div>
       <div className={'row-span-11 flex-col flex m-6'}>
         <Typography type="subtitle" fontWeight="bold">
           {swimmingPool.name}
         </Typography>
-        {!_expanded || (swimmingPool.expandedDescription && swimmingPool.waterTemp) ? (
+        {swimmingPool.expandedDescription && swimmingPool.waterTemp ? (
           <div className="flex-1">
-            <p className="my-4 whitespace-pre-wrap">
-              {_expanded ? swimmingPool.expandedDescription : swimmingPool.description}
-            </p>
+            <p className="my-4 whitespace-pre-wrap">{swimmingPool.description}</p>
           </div>
         ) : (
           <div className="flex flex-1 flex-col items-center justify-center text-xl">
