2d2185c2608809a9460bd7b430e0e1fd6e5a46d4
Adam Grund <adam.grund@gmail.com>
2023-06-12 15:55:28 +0200

    [#105] add SwimmingPools back on landing page (#109)
    * [#105] add SwimmingPools back on landing page
    
    * [#105] removed usless code

---
+++ src/components/SwimmingPoolInfoCard/SwimmingPoolInfoCard.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { SwimmingPool } from 'models'
+
+import { Typography, Spinner } from 'components'
+
+import './SwimmingPoolInfoCard.css'
+import { useTranslation } from 'react-i18next'
+
+interface SwimmingPoolInfoCardProps {
+  swimmingPool: SwimmingPool
+  className?: string
+}
+
+const SwimmingPoolInfoCard = ({ swimmingPool, className = '' }: SwimmingPoolInfoCardProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <div className={`${className} grid bg-backgroundGray rounded-lg swimming-pool-card__container`}>
+      <div className={`sm:rounded-none row-span-1  relative`}>
+        <img
+          className={`swimming-pool-card__image rounded-t-lg relative`}
+          src={swimmingPool.image ? swimmingPool.image.originalFile : '/kupalisko-delfin.png'}
+          alt={swimmingPool.image ? swimmingPool.image.altText : t('landing.alt-img-text')}
+        />
+      </div>
+      <div className={'row-span-11 flex-col flex m-6'}>
+        <Typography type="subtitle" fontWeight="bold">
+          {swimmingPool.name}
+        </Typography>
+        {swimmingPool.expandedDescription && swimmingPool.waterTemp ? (
+          <div className="flex-1">
+            <p className="my-4 whitespace-pre-wrap">{swimmingPool.description}</p>
+          </div>
+        ) : (
+          <div className="flex flex-1 flex-col items-center justify-center text-xl">
+            <div className="w-1/10 py-4">
+              <Spinner />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default SwimmingPoolInfoCard
--- src/helpers/general.ts
+++ src/helpers/general.ts
@@ -2,6 +2,22 @@ import i18n from '../i18n'
 import { useTranslation } from 'react-i18next'
 import { TicketFromHistory } from 'store/tickets-history/api'
 
+export const assignItemsToColumns = (numberOfColumns: 1 | 2 | 3, items: any[]): any[][] => {
+  const result: any[][] = Array.from({ length: numberOfColumns }, () => [])
+  if (numberOfColumns === 3 && items.length % 3 === 1) {
+    items.forEach((item, index) => {
+      if (index === items.length - 1) {
+        result[1].push(item)
+      } else {
+        result[index % numberOfColumns].push(item)
+      }
+    })
+  } else {
+    items.forEach((item, index) => result[index % numberOfColumns].push(item))
+  }
+  return result
+}
+
 export const getPrice = (price: number) => Math.floor(price * 100) / 100
 
 export const convertBase64ToBlob = (base64Image: string, contentType?: string) => {
