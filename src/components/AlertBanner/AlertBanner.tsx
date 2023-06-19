5b59e4c2ef62d18fd9f954f983055ad9a56609d0
Adam Grund <adam.grund@gmail.com>
2023-06-19 10:45:46 +0200

    [#116] change AlertBanner render conditions

--- src/components/AlertBanner/AlertBanner.tsx
+++ src/components/AlertBanner/AlertBanner.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Icon } from 'components'
-import { useTranslation } from 'react-i18next'
 
-const AlertBanner = () => {
-  const { t } = useTranslation()
+interface AlertBannerProps {
+  text: string
+}
+
+const AlertBanner = ({ text }: AlertBannerProps) => {
+  if (text === '') return null
 
   return (
     <div className="bg-[#E07B04] text-white flex">
       <div className="container flex gap-3 py-3 lg:items-center lg:py-4 mx-auto">
         <Icon className="flex items-center" name="alert" />
-        <div className="grow">{t('landing.alert-text')}</div>
+        <div className="grow">{text}</div>
       </div>
     </div>
   )
