44e731e11c186f0f207ecc82e09f91025e1a74ec
Adam Grund <adam.grund@gmail.com>
2024-06-11 13:38:39 +0200

    [#183] styling landing page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -71,9 +71,7 @@ const Header = () => {
     <>
       <div className="bg-sunscreen shadow-xs py-3 md:py-4 sticky top-0 z-40 w-full header">
         <div className="container mx-auto text-fontBlack flex justify-between">
-          <Link className="text-primary font-bold text-xl cursor-pointer" to="/">
-            STARZ
-          </Link>
+          <Icon name="starz-logo" className="hidden xs:block no-fill pr-5" />
           <nav className="hidden md:flex flex-1 items-center justify-end">
             {menuItems.map((menuItem, index) => (
               <div key={menuItem.to} className={cx('flex', { relative: menuItem.icon })}>
