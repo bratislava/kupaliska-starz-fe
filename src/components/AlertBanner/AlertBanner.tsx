65fe4c091d830da536806e8fae8bb8f77cd6fdb1
Adam Grund <adam.grund@gmail.com>
2024-06-20 13:10:22 +0200

    add AlertBanner text (#190)

--- src/components/AlertBanner/AlertBanner.tsx
+++ src/components/AlertBanner/AlertBanner.tsx
@@ -9,7 +9,7 @@ const AlertBanner = ({ text }: AlertBannerProps) => {
   if (text === '') return null
 
   return (
-    <div className="bg-[#E07B04] text-white flex">
+    <div className="bg-[#1F1F1F] text-white flex">
       <div className="container flex gap-3 py-3 lg:items-center lg:py-4 mx-auto">
         <Icon className="flex items-center" name="alert" />
         <div className="grow">{text}</div>
