2c9fa2dc74adb7192d89b58cd2f28bb98b11cd68
Adam Grund <adam.grund@gmail.com>
2025-07-24 11:37:58 +0200

    change text positioning

--- src/components/HomepageTickets/HomepageTickets.tsx
+++ src/components/HomepageTickets/HomepageTickets.tsx
@@ -52,80 +52,84 @@ const HomepageTickets = () => {
   }
 
   return (
-    <div className="flex flex-col gap-8 lg:gap-10">
-      <span>{t('common.additional-info-toddlers')}</span>
-      {[
-        {
-          name: 'Jednorazové lístky',
-          description:
-            'Vhodné pre príležitostných návštevníkov alebo pre tých, ktorí nechcú čakať pred kúpaliskom v dlhom rade a kúpia si lístok online priamo na mieste.',
-          descriptionFooter: t('common.additional-info-student-senior'),
-          tickets: dayTickets,
-        },
-        {
-          name: 'Vstupové permanentky',
-          description:
-            'Platí na 10 vstupov počas celej sezóny bez ohľadu na vek. Jedna permanentka je viazaná na jednu osobu a je neprenosná.',
-          descriptionFooter: '',
-          tickets: entryTickets,
-        },
-        {
-          name: 'Sezónne permanentky',
-          description:
-            'Neobmedzený vstup počas celej sezóny na všetky naše kúpaliská a 90 minútový vstup denne na Mestskú Plaváreň Pasienky. K sezónnej permanentke pre dospelých a ŤZP/ŤZP-S je možné zakúpiť detskú permanentku až pre 3 deti za zvýhodnenú cenu 9,90 € za dieťa.',
-          descriptionFooter: '',
-          tickets: seasonalTickets,
-        },
-      ].map(({ name, description, descriptionFooter, tickets }, index) => (
-        <div key={index} className="max-w-[904px]">
-          <div className="flex flex-col gap-8">
-            <div className="flex flex-col gap-3 text-center lg:text-left">
-              <h5 className="font-semibold text-xl">{name}</h5>
-              <p>{description}</p>
-            </div>
-            <div className="flex flex-col gap-3">
-              {tickets?.map((ticket) => {
-                const needsLogin = ticketNeedsLogin(ticket)
+    <>
+      <div className="flex flex-col gap-8 lg:gap-10">
+        {[
+          {
+            name: 'Jednorazové lístky',
+            description:
+              'Vhodné pre príležitostných návštevníkov alebo pre tých, ktorí nechcú čakať pred kúpaliskom v dlhom rade a kúpia si lístok online priamo na mieste.',
+            descriptionFooter: t('common.additional-info-student-senior'),
+            tickets: dayTickets,
+          },
+          {
+            name: 'Vstupové permanentky',
+            description:
+              'Platí na 10 vstupov počas celej sezóny bez ohľadu na vek. Jedna permanentka je viazaná na jednu osobu a je neprenosná.',
+            descriptionFooter: '',
+            tickets: entryTickets,
+          },
+          {
+            name: 'Sezónne permanentky',
+            description:
+              'Neobmedzený vstup počas celej sezóny na všetky naše kúpaliská a 90 minútový vstup denne na Mestskú Plaváreň Pasienky. K sezónnej permanentke pre dospelých a ŤZP/ŤZP-S je možné zakúpiť detskú permanentku až pre 3 deti za zvýhodnenú cenu 9,90 € za dieťa.',
+            descriptionFooter: '',
+            tickets: seasonalTickets,
+          },
+        ].map(({ name, description, descriptionFooter, tickets }, index) => (
+          <div key={index} className="max-w-[904px]">
+            <div className="flex flex-col gap-8">
+              <div className="flex flex-col gap-3 text-center lg:text-left">
+                <h5 className="font-semibold text-xl">{name}</h5>
+                <p>{description}</p>
+              </div>
+              <div className="flex flex-col gap-3">
+                {tickets?.map((ticket) => {
+                  const needsLogin = ticketNeedsLogin(ticket)
 
-                return (
-                  <div
-                    key={ticket.id}
-                    className={cx(
-                      'px-6 py-4 rounded-lg flex flex-col lg:flex-row gap-8 border border-divider lg:items-center bg-sunscreen',
-                      { 'cursor-pointer': !ticket.disabled },
-                    )}
-                    onClick={() => handleClick(ticket)}
-                  >
-                    <span className="grow font-semibold">{ticket.name}</span>
-                    <div className="flex items-center justify-between gap-x-8">
-                      <span className="lg:w-[108px] font-semibold lg:text-right">
-                        {currencyFormatter.format(ticket.priceWithVat)}
-                      </span>
-                      <Button
-                        className="xs:px-4 w-full mt-2 xs:mt-0 xs:w-auto min-w-[182px]"
-                        thin
-                        onClick={() => handleClick(ticket)}
-                        color={needsLogin ? 'primary' : 'outlined'}
-                        disabled={ticket.disabled}
-                      >
-                        {needsLogin ? t('signin-button') : t('landing.basket')}
-                        <Icon
-                          name={needsLogin ? 'login' : 'euro-coin'}
-                          className={cx('ml-2 no-fill', {
-                            'py-1': !needsLogin,
-                          })}
-                        />
-                      </Button>
+                  return (
+                    <div
+                      key={ticket.id}
+                      className={cx(
+                        'px-6 py-4 rounded-lg flex flex-col lg:flex-row gap-8 border border-divider lg:items-center bg-sunscreen',
+                        { 'cursor-pointer': !ticket.disabled },
+                      )}
+                      onClick={() => handleClick(ticket)}
+                    >
+                      <span className="grow font-semibold">{ticket.name}</span>
+                      <div className="flex items-center justify-between gap-x-8">
+                        <span className="lg:w-[108px] font-semibold lg:text-right">
+                          {currencyFormatter.format(ticket.priceWithVat)}
+                        </span>
+                        <Button
+                          className="xs:px-4 w-full mt-2 xs:mt-0 xs:w-auto min-w-[182px]"
+                          thin
+                          onClick={() => handleClick(ticket)}
+                          color={needsLogin ? 'primary' : 'outlined'}
+                          disabled={ticket.disabled}
+                        >
+                          {needsLogin ? t('signin-button') : t('landing.basket')}
+                          <Icon
+                            name={needsLogin ? 'login' : 'euro-coin'}
+                            className={cx('ml-2 no-fill', {
+                              'py-1': !needsLogin,
+                            })}
+                          />
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                )
-              })}
+                  )
+                })}
+              </div>
+              {descriptionFooter && <p className="text-sm">{descriptionFooter}</p>}
             </div>
-            {descriptionFooter && <p className="text-sm">{descriptionFooter}</p>}
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div className="flex flex-col text-center my-8 text-sm leading-loose">
+        <span>{t('common.additional-info-toddlers')}</span>
+      </div>
+    </>
   )
 }
 
