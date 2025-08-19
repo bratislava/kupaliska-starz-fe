1e9a9e81e9ad827f9dc0c88613fccf2ea18241d9
Adam Grund <adam.grund@gmail.com>
2025-08-19 09:58:26 +0200

    disable sales of entry ticket with name (#276)

--- src/components/HomepageTickets/HomepageTickets.tsx
+++ src/components/HomepageTickets/HomepageTickets.tsx
@@ -15,7 +15,7 @@ const partitionTickets = (tickets: Ticket[]) => ({
   dayTickets: tickets.filter((ticket) => ticket.type === 'ENTRIES' && !ticket.nameRequired),
   entryTickets: tickets
     .filter((ticket) => ticket.type === 'ENTRIES' && ticket.nameRequired)
-    .map((ticket) => ({ ...ticket, disabled: false })),
+    .map((ticket) => ({ ...ticket, disabled: true })),
   seasonalTickets: tickets
     .filter((ticket) => ticket.type === 'SEASONAL')
     .map((ticket) => ({ ...ticket, disabled: true })),
