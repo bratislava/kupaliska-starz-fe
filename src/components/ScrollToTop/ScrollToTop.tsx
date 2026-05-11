c5ea7c8d7c909dbd2b5516fddc6ae79137dd629c
Adam Grund <adam.grund@gmail.com>
2026-05-11 16:46:15 +0200

    add comment

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -4,7 +4,11 @@ import { useLocation } from 'react-router'
 function ScrollToTop({ children }: PropsWithChildren<{}>) {
   const location = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0)
+    // this is not playing nice with react-router-hash-link, 
+    // example: when you click on hashlink (kupaliska) on homepage it will not scroll to that section,
+    // when commenting this line it will scroll but it will not scroll to top when changing route
+    // TODO: fix this
+    // window.scrollTo(0, 0)
 
   }, [location])
 
