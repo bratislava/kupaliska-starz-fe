913d611dc7809e5bba7541611025568d29c554a2
Adam Grund <adam.grund@gmail.com>
2026-06-22 20:07:01 +0200

    change senior age

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1053,7 +1053,7 @@ const OrderPage = () => {
                   error={errorSeniorAgreementInterpreted}
                   label={
                     <span>
-                      Potvrdzujem, že všetky dospelé osoby v nákupnom košíku majú 65 a viac rokov
+                      Potvrdzujem, že všetky dospelé osoby v nákupnom košíku majú 64 a viac rokov
                       alebo sú držitelia ŤZP / ŤZP-S preukazu.
                     </span>
                   }
