ee2b995211e09dee20fa30254328af1aaa479a33
Adam Grund <adam.grund@gmail.com>
2026-07-21 22:37:33 +0200

    add `prettier-plugin-tailwindcss` to devDependencies

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,8 +2,8 @@ import { prettierBase } from '@bratislava/eslint-config-react'
 
 export default {
   ...prettierBase,
+  plugins: ['prettier-plugin-tailwindcss'],
   // TODO add packages below, taken from https://github.com/bratislava/eslint-config/tree/master/packages/react#prettier
-  // plugins: ["prettier-plugin-tailwindcss"],
   // tailwindFunctions: ["clsx", "cn"],
   // project-specific:
 }
