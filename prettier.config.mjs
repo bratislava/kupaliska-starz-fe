1056179cb93ce5d1044d3d33829f6d72b62ba39b
Adam Grund <adam.grund@gmail.com>
2026-07-21 19:56:31 +0200

    add eslint from @bratislava/eslint-config-react

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+import { prettierBase } from "@bratislava/eslint-config-react";
+
+export default {
+  ...prettierBase,
+  // TODO add packages below, taken from https://github.com/bratislava/eslint-config/tree/master/packages/react#prettier
+  // plugins: ["prettier-plugin-tailwindcss"],
+  // tailwindFunctions: ["clsx", "cn"],
+  // project-specific:
+};
