38dcc545c7fe2c04bd615c91be2547ac72175644
Adam Grund <adam.grund@gmail.com>
2026-07-29 15:36:00 +0200

    migrate manually tailwind classes

--- src/components/Switch/KupaliskaSwitch.tsx
+++ src/components/Switch/KupaliskaSwitch.tsx
@@ -1,20 +1,15 @@
 import { ComponentProps } from 'react'
 import Switch from 'react-switch'
 
-declare const preval: (code: TemplateStringsArray) => Record<string, unknown>
-
-const { onColor, offColor } = preval`
-  const tailwindConfig = require('../../../tailwind.config');
-  const colors = tailwindConfig.theme.extend.colors;
-
-  module.exports = {onColor: colors.primary, offColor: colors.inactive}
-` as { onColor: string; offColor: string }
+// not a best solution but working one for now
+const getThemeColor = (name: string) =>
+  getComputedStyle(document.documentElement).getPropertyValue(`--color-${name}`).trim()
 
 const KupaliskaSwitch = (props: ComponentProps<typeof Switch>) => {
   return (
     <Switch
-      onColor={onColor}
-      offColor={offColor}
+      onColor={getThemeColor('primary')}
+      offColor={getThemeColor('inactive')}
       height={24}
       width={48}
       handleDiameter={16}
