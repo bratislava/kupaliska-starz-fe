634da5112f4cbab72f5012acc6078e25f001a7b0
Adam Grund <adam.grund@gmail.com>
2026-07-29 15:45:05 +0200

    add index.css to configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,5 +7,11 @@ export default [
       'better-tailwindcss/enforce-consistent-line-wrapping': 'off',
       'better-tailwindcss/no-unknown-classes': ['warn', { ignore: ['no-fill'] }],
     },
+    settings: {
+      'better-tailwindcss': {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        entryPoint: 'src/index.css',
+      },
+    },
   },
 ]
