3c78505d78d7daf0a29d743c1837df09ee94d676
Adam Grund <adam.grund@gmail.com>
2026-09-02 14:47:00 +0200

    change suspense text for skeleton loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import './i18n'
 import './index.css'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import { Provider } from 'react-redux'
 
 import App from './App'
@@ -16,7 +18,21 @@ const root = createRoot(container)
 
 root.render(
   <StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <SkeletonTheme
+          baseColor="#a8dbf2"
+          highlightColor="#58bbe6"
+          duration={1}
+          width={40}
+          height={28}
+        >
+          <div className="flex h-full items-center justify-center">
+            <Skeleton />
+          </div>
+        </SkeletonTheme>
+      }
+    >
       <Provider store={store}>
         <App />
       </Provider>
